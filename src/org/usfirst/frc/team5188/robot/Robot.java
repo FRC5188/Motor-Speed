07874d3423154431073924ac18ae5e80694b3dbe
slcrowell <slcrowell3@gmail.com>
2017-01-16 17:35:52 -0500

    Done Changed
    Do work

--- src/org/usfirst/frc/team5188/robot/Robot.java
+++ src/org/usfirst/frc/team5188/robot/Robot.java
@@ -1,11 +1,13 @@
 
 package org.usfirst.frc.team5188.robot;
 
+import java.util.Arrays;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import java.lang.System;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -18,6 +20,11 @@ public class Robot extends IterativeRobot {
     final String customAuto = "My Auto";
     String autoSelected;
     SendableChooser chooser;
+    Sendable thing;
+    Preferences pref;
+    
+    double p, i, d;
+    
 	
     SuperJoystickPlus controller;
     Shooter shooter;
@@ -31,9 +38,12 @@ public class Robot extends IterativeRobot {
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", defaultAuto);
         chooser.addObject("My Auto", customAuto);
+        pref = Preferences.getInstance();
+        
+        
         SmartDashboard.putData("Auto choices", chooser);
         controller = new SuperJoystickPlus(0);
-        shooter = new Shooter(0, .00007, 0.0000003, -0.00000001, 20, true);
+        shooter = new Shooter(0, .00006, 0.000008, -0.000000001, 10, true);
         controller.setDeadzone(.01);
     }
     
@@ -73,16 +83,24 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
         shooter.setThrottle(controller.get(CTRL_AXIS.LY));
         counter ++;
-		System.out.println("IN PID LOOP: " + " Speed: " + shooter.read() + " Set Point: " + shooter.getSetPoint() + " Error: " + shooter.controller.error + " Motor Throttle: " + shooter.lastSet);
+		System.out.println(" Speed: " + shooter.read() + " Set Point: " + shooter.getSetPoint() + " Error: " + shooter.controller.error + " Motor Throttle: " + shooter.lastSet);
         if(controller.isButtonPushed(CTRL_BTN.A)){
 //        	int counter = 0;
-//        	shooter.start_pid();
+        	shooter.start_pid();
         	while(!controller.isButtonPushed(CTRL_BTN.B)){
         		counter ++;
-        		if((counter % 1000) == 0){System.out.println("IN PID LOOP: " + "Throttle: " + controller.get(CTRL_AXIS.LY) + " Speed: " + shooter.read() + " Set Point: " + shooter.getSetPoint() + " Error: " + shooter.controller.error + " Motor Throttle: " + shooter.lastSet);}
-        		shooter.setRPM(controller.get(CTRL_AXIS.LY) * 1000);
-        		shooter.controller.runIteration();
-//        		System.out.println("Throttle: " + controller.get(CTRL_AXIS.LY) + " Speed: " + shooter.read() + " Set Point: " + shooter.getSetPoint());
+        		if((counter % 10000) == 0){
+        			System.out.println("IN PID LOOP: " + "Throttle: " + controller.get(CTRL_AXIS.LY) + " Speed: " + shooter.read() + " Set Point: " + shooter.getSetPoint() + " Error: " + shooter.controller.error + " Motor Throttle: " + shooter.lastSet + "PIDs are: " + Arrays.toString(shooter.controller.getPIDS()));
+        		}
+        		shooter.setRPM(2675);
+        		pref = Preferences.getInstance();
+        		double[] temp = {pref.getDouble("P", .00006), pref.getDouble("I",  0.000008), pref.getDouble("D", -0.000000001)};
+        		if(!Arrays.equals(shooter.controller.getPIDS(),temp)){
+        			System.out.println("Changing PIDS to: " + Arrays.toString(temp));
+        			shooter.controller.setPIDS(temp[0], temp[1], temp[2]);
+        		}
+//        		if(controller.get(CTRL_AXIS.LY) == 0){shooter.controller.resetAccumulator();}
+//        		shooter.setRPM(controller.get(CTRL_AXIS.LY) * 1700);
         	}
         	shooter.stop();
         }
